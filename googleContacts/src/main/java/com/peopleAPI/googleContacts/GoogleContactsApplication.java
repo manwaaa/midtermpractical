bf93590c92b0cd618ebd96a3141e53bd1b33b772
unknown <manwarulfrancis.santos@cit.edu>
2025-03-04 23:01:23 +0800

    initial commit

---
+++ googleContacts/src/main/java/com/peopleAPI/googleContacts/GoogleContactsApplication.java
@@ -0,0 +1,13 @@
+package com.peopleAPI.googleContacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleContactsApplication.class, args);
+	}
+
+}
---
+++ googleContacts/src/test/java/com/peopleAPI/googleContacts/GoogleContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peopleAPI.googleContacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
